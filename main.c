bbdb2dde34b320bce5f95d1fcb2ecf04d4197b2a
Pablo Otero <potero91@gmail.com>
2022-03-11 12:39:25 +0100

    exit

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: potero-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 12:56:10 by potero-d          #+#    #+#             */
-/*   Updated: 2022/03/11 11:58:54 by potero-d         ###   ########.fr       */
+/*   Updated: 2022/03/11 12:39:01 by potero-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,45 @@
 #include <readline/history.h>
 #include "minishell.h"
 
-/*
-int	main(int argc, char *argv[])
+int	min_stop(char *str)
 {
-	char	**str;
-	int 	i;
-	
-	i = 0;
-	if (argc > 1)
+	if (ft_strlen(str) != 4)
+		return (1);
+	else
 	{
-		str = ft_split(argv[1], ' ');
-		while (str[i])
-		{
-			printf("%s\n", str[i]);
-			i++;
-		}
+		if (str[0] != 'e')
+			return (1);
+		else if (str[1] != 'x')
+			return (1);
+		else if (str[2] != 'i')
+			return (1);
+		else if (str[3] != 't')
+			return (1);
 	}
 	return (0);
 }
-*/
+
 int	main(void)
 {
 	char	*str;
-	while (1)
+	char	**split;
+	int		i;
+	int		stop;
+
+	stop = 1;
+	while (stop != 0)
 	{
 		str = readline("Minishell$ ");
-		printf("you type %s\n", str);
+		if (min_stop(str) == 0)
+			stop = 0;
+		split = ft_split(str, ' ');
+		i = 0;
+		while (split[i])
+		{
+			printf("%s\n", split[i]);
+			free(split[i]);
+			i++;
+		}
 		free(str);
 	}
 	return (0);
