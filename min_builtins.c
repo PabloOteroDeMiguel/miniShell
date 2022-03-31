d857c3f4e2920da6aeff513d83fbbdade57c8d63
Pablo Otero <potero91@gmail.com>
2022-03-31 11:59:45 +0200

    echo

--- min_builtins.c
+++ min_builtins.c
@@ -6,7 +6,7 @@
 /*   By: potero-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 11:13:18 by potero-d          #+#    #+#             */
-/*   Updated: 2022/03/31 11:38:53 by potero-d         ###   ########.fr       */
+/*   Updated: 2022/03/31 11:57:51 by potero-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,46 @@ void	min_echo(t_argv **argv)
 {
 	t_argv	*aux;
 	int		i;
+	int		n;
 	
+	n = 0;
 	aux = *argv;
-	if	(ft_strcmp(aux->split[0], "echo") == 0)
+	if (aux->split[1])
 	{
-		if (aux->split[1])
+		if (ft_strcmp(aux->split[1], "-n") == 0)
 		{
-			if (ft_strcmp(aux->split[1], "-n") == 0)
-				i = 2;
-			else
-				i = 1;
-			while (aux->split[i])
-			{
-				printf("%s ", aux->split[i]);
-				i++;
-			}
+			n = 1;
+			i = 2;
+		}
+		else
+			i = 1;
+		while (aux->split[i])
+		{
+			printf("%s ", aux->split[i]);
+			i++;
+		}
+		aux = aux->next;
+		while (aux)
+		{
+			printf("%s", aux->arg);
 			aux = aux->next;
-			while (aux)
-			{
-				printf("%s", aux->arg);
-				aux = aux->next;
-			}
 		}
 	}
-
+	else
+	{
+		aux = aux->next;
+		if (ft_strcmp(aux->arg, "-n") == 0)
+		{
+			n = 1;
+			aux = aux->next;
+		}
+		while (aux)
+		{
+			printf("%s", aux->arg);
+			aux = aux->next;
+		}
+	}
+	if (n == 0)
+		printf("\n");
 }
 
