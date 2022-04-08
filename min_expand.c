a6c46c5ae36ce8c17ca9b8a2795d4210c43fe65f
Pablo Otero <potero91@gmail.com>
2022-04-08 16:07:42 +0200

    expand with single quotes

--- min_expand.c
+++ min_expand.c
@@ -6,32 +6,48 @@
 /*   By: potero-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 11:13:41 by potero-d          #+#    #+#             */
-/*   Updated: 2022/04/08 15:40:52 by potero-d         ###   ########.fr       */
+/*   Updated: 2022/04/08 16:07:11 by potero-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+
 char	*change_str(char *str, t_myenv **myenv)
 {
 	t_myenv	*aux;
 	char	*key;
+	char	*quote;
+	char	*aux1;
 	char	*print;
 	char	*sub;
 	int		i;
 
 	aux = *myenv;
+	quote = 0;
 	key = ft_strchr(str, '$') + 1;
 	i = 0;
 	while (str[i] != '$')
 		i++;
 	sub = ft_substr(str, 0, i);
+	if (ft_strchr(key, 39) != 0)
+	{
+		aux1= ft_strchr(key, 39);
+		quote = ft_strdup(aux1);
+		i = 0;
+		while (key[i] != 39)
+			i++;
+		key[i] = 0;
+	}
 	while (aux)
 	{
 		if (ft_strcmp(key, aux->key) == 0)
 		{
-			print = ft_strjoin(sub, aux->value);
+			aux1 = ft_strjoin(sub, aux->value);
 			free(sub);
+			print = ft_strjoin(aux1, quote);
+			free(aux1);
+			free(quote);
 			return (print);
 		}
 		aux = aux->next;
